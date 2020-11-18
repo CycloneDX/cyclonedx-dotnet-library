8acb78ca206edfae0036a23d49114849c95fda44
Patrick Dwyer <coderpatros@outlook.com>
2020-11-19 06:36:42 +1000

    Refactor json tests to be version specific

--- CycloneDX.Json.Tests/Tests.cs
+++ CycloneDX.Json.Tests/Tests.cs
@@ -11,31 +11,31 @@ namespace CycloneDX.Json.Tests
     {
         [Theory]
         [InlineData("bom")]
-        [InlineData("valid-component-hashes-1.2")]
-        [InlineData("valid-component-ref-1.2")]
-        [InlineData("valid-component-swid-1.2")]
-        [InlineData("valid-component-swid-full-1.2")]
-        [InlineData("valid-component-types-1.2")]
-        [InlineData("valid-dependency-1.2")]
-        [InlineData("valid-empty-components-1.2")]
-        [InlineData("valid-license-expression-1.2")]
-        [InlineData("valid-license-id-1.2")]
-        [InlineData("valid-license-name-1.2")]
-        [InlineData("valid-metadata-author-1.2")]
-        [InlineData("valid-metadata-manufacture-1.2")]
-        [InlineData("valid-metadata-supplier-1.2")]
-        [InlineData("valid-metadata-timestamp-1.2")]
-        [InlineData("valid-metadata-tool-1.2")]
-        [InlineData("valid-minimal-viable-1.2")]
-        [InlineData("valid-patch-1.2")]
-        [InlineData("valid-service-1.2")]
-        [InlineData("valid-service-empty-objects-1.2")]
-        public void JsonRoundTripTest(string filename)
+        [InlineData("valid-component-hashes")]
+        [InlineData("valid-component-ref")]
+        [InlineData("valid-component-swid")]
+        [InlineData("valid-component-swid-full")]
+        [InlineData("valid-component-types")]
+        [InlineData("valid-dependency")]
+        [InlineData("valid-empty-components")]
+        [InlineData("valid-license-expression")]
+        [InlineData("valid-license-id")]
+        [InlineData("valid-license-name")]
+        [InlineData("valid-metadata-author")]
+        [InlineData("valid-metadata-manufacture")]
+        [InlineData("valid-metadata-supplier")]
+        [InlineData("valid-metadata-timestamp")]
+        [InlineData("valid-metadata-tool")]
+        [InlineData("valid-minimal-viable")]
+        [InlineData("valid-patch")]
+        [InlineData("valid-service")]
+        [InlineData("valid-service-empty-objects")]
+        public void JsonRoundTripTest_v1_2(string filename)
         {
-            var resourceFilename = Path.Join("Resources", filename + ".json");
+            var resourceFilename = Path.Join("Resources", filename + "-1.2.json");
             var jsonBom = File.ReadAllText(resourceFilename);
 
-            var bom = JsonBomDeserializer.Deserialize(jsonBom);
+            var bom = JsonBomDeserializer.Deserialize_v1_2(jsonBom);
             jsonBom = JsonBomSerializer.Serialize(bom);
 
             Snapshot.Match(jsonBom, SnapshotNameExtension.Create(filename));
