bf86c26fac999cfc3e1e0e7cb112f2b01aa78b5f
Jim Klimov <jimklimov@gmail.com>
2023-08-07 14:15:08 +0200

    Restore simplistic CycloneDX.Utils/Merge.cs logic for ListMergeHelper: move the BomEntity related complexity to CycloneDX.Core/BomUtils.cs (initially "as is")
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- src/CycloneDX.Utils/Merge.cs
+++ src/CycloneDX.Utils/Merge.cs
@@ -27,120 +27,22 @@ namespace CycloneDX.Utils
     {
         public List<T> Merge(List<T> list1, List<T> list2)
         {
-            if (!int.TryParse(System.Environment.GetEnvironmentVariable("CYCLONEDX_DEBUG_MERGE"), out int iDebugLevel) || iDebugLevel < 0)
-                iDebugLevel = 0;
+            if (list1 is null || list1.Count < 1) return list2;
+            if (list2 is null || list2.Count < 1) return list1;
 
-            if (iDebugLevel >= 1)
-                Console.WriteLine($"List-Merge for: {this.GetType().ToString()}");
-            if (list1 is null) return list2;
-            if (list2 is null) return list1;
-
-            List<T> result = new List<T>(list1);
-
-            var TType = ((T)list2[0]).GetType();
-            var methodMergeWith = TType.GetMethod("mergeWith");
-            var methodEquals = TType.GetMethod("Equals", 0, new Type[] { TType });
-
-            foreach (var item2 in list2)
+            if (((T)list2[0]).GetType() is BomEntity)
             {
-                bool isContained = false;
-                if (iDebugLevel >= 3)
-                    Console.WriteLine($"result<{TType.ToString()}> now contains {result.Count} entries");
-                for (int i=0; i < result.Count; i++)
-                {
-                    if (iDebugLevel >= 3)
-                        Console.WriteLine($"result<{TType.ToString()}>: checking entry #{i}");
-                    T item1 = result[i];
-                    // Squash contents of the new entry with an already
-                    // existing equivalent (same-ness is subject to
-                    // IEquatable<>.Equals() checks defined in respective
-                    // classes), if there is a method defined there:
-                    if (methodMergeWith != null)
-                    {
-                        try
-                        {
-                            if (((bool)methodMergeWith.Invoke(item1, new object[] {item2})))
-                            {
-                                isContained = true;
-                                break; // item2 merged into result[item1] or already equal to it
-                            }
-                        }
-                        catch (System.Exception exc)
-                        {
-                            if (iDebugLevel >= 1)
-                                Console.WriteLine($"SKIP MERGE: can not mergeWith() {item1.ToString()} and {item2.ToString()}: {exc.ToString()}");
-                        }
-                    } // else: That class lacks a mergeWith(), gotta trust the equality
-                    else
-                    {
-                        if (iDebugLevel >= 6)
-                            Console.WriteLine($"SKIP MERGE? can not mergeWith() {item1.ToString()} and {item2.ToString()}: no such method");
-                        if (item1 is IEquatable<T>)
-                        {
-                            if (methodEquals != null)
-                            {
-                                try
-                                {
-                                    if (iDebugLevel >= 5)
-                                        Console.WriteLine($"LIST-MERGE: try methodEquals()");
-                                    if (((bool)methodEquals.Invoke(item1, new object[] {item2})))
-                                    {
-                                        isContained = true;
-                                        break;
-                                    }
-                                }
-                                catch (System.Exception exc)
-                                {
-                                    if (iDebugLevel >= 5)
-                                        Console.WriteLine($"LIST-MERGE: can not check Equals() {item1.ToString()} and {item2.ToString()}: {exc.ToString()}");
-                                }
-                            }
+                return BomUtils.MergeBomEntityLists(list1, list2);
+            }
 
-                            if (item1.Equals(item2))
-                            {
-                                // Fall back to generic equality check which may be useless
-                                if (iDebugLevel >= 3)
-                                    Console.WriteLine($"SKIP MERGE: items say they are equal");
-                                isContained = true;
-                                break; // items deemed equivalent
-                            }
+			// Lists of legacy types
+            var result = new List<T>(list1);
 
-                            if (iDebugLevel >= 3)
-                                Console.WriteLine($"MERGE: items say they are not equal");
-                        }
-                        else
-                        {
-                            if (iDebugLevel >= 3)
-                                Console.WriteLine($"MERGE: items are not IEquatable");
-                        }
-/*
-                        else
-                        {
-                            if (item1 is CycloneDX.Models.Bom)
-                            {
-                                if (CycloneDX.Json.Serializer.Serialize((CycloneDX.Models.Bom)item1) == CycloneDX.Json.Serializer.Serialize((CycloneDX.Models.Bom)item2))
-                                {
-                                    isContained = true;
-                                    break; // items deemed equivalent
-                                }
-                            }
-                        }
-*/
-                    }
-                }
-
-                if (!isContained)
+            foreach (var item in list2)
+            {
+                if (!(result.Contains(item)))
                 {
-                    // Add new entry "as is" (new-ness is subject to
-                    // equality checks of respective classes):
-                    if (iDebugLevel >= 2)
-                        Console.WriteLine($"WILL ADD: {item2.ToString()}");
-                    result.Add(item2);
-                }
-                else
-                {
-                    if (iDebugLevel >= 2)
-                        Console.WriteLine($"ALREADY THERE: {item2.ToString()}");
+                    result.Add(item);
                 }
             }
 
