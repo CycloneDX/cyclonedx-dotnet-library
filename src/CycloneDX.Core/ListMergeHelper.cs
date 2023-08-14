1f4c12b843e1b01d1054fb8b4decb6d48c843b04
Jim Klimov <jimklimov@gmail.com>
2023-08-14 21:39:24 +0200

    BomEntity.cs, ListMergeHelper.cs: avoid syntax that requires newer C#
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- src/CycloneDX.Core/ListMergeHelper.cs
+++ src/CycloneDX.Core/ListMergeHelper.cs
@@ -54,11 +54,11 @@ namespace CycloneDX
             // Rule out utterly empty inputs
             if ((list1 is null || list1.Count < 1) && (list2 is null || list2.Count < 1))
             {
-                if (list1 is not null)
+                if (!(list1 is null))
                 {
                     return list1;
                 }
-                if (list2 is not null)
+                if (!(list2 is null))
                 {
                     return list2;
                 }
