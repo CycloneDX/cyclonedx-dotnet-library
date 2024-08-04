e516bdb428f7164fb3b1e5a413b4761177f5021c
andreas-hilti <69210561+andreas-hilti@users.noreply.github.com>
2024-08-04 21:30:52 +0200

    Fix metadata tools components (#304)
    * Fix metadata tools components
    
    Signed-off-by: andreas hilti <andreas.hilti@bluewin.ch>
    
    * namespace tool components and services
    
    Signed-off-by: andreas hilti <andreas.hilti@bluewin.ch>
    
    * avoid writing null value in tools
    
    Signed-off-by: andreas hilti <andreas.hilti@bluewin.ch>
    
    * namespace also nested tools components
    
    Signed-off-by: andreas hilti <andreas.hilti@bluewin.ch>
    
    ---------
    
    Signed-off-by: andreas hilti <andreas.hilti@bluewin.ch>

--- src/CycloneDX.Utils/Merge.cs
+++ src/CycloneDX.Utils/Merge.cs
@@ -91,13 +91,19 @@ namespace CycloneDX.Utils
             var toolsMerger = new ListMergeHelper<Tool>();
             #pragma warning restore 618
             var tools = toolsMerger.Merge(bom1.Metadata?.Tools?.Tools, bom2.Metadata?.Tools?.Tools);
-            if (tools != null)
+            var toolsComponentsMerger = new ListMergeHelper<Component>();
+            var toolsComponents = toolsComponentsMerger.Merge(bom1.Metadata?.Tools?.Components, bom2.Metadata?.Tools?.Components);
+            var toolsServicesMerger = new ListMergeHelper<Service>();
+            var toolsServices = toolsServicesMerger.Merge(bom1.Metadata?.Tools?.Services, bom2.Metadata?.Tools?.Services);
+            if (tools != null || toolsComponents != null || toolsServices != null)
             {
                 result.Metadata = new Metadata
                 {
                     Tools = new ToolChoices
                     {
                         Tools = tools,
+                        Components = toolsComponents,
+                        Services = toolsServices,
                     }
                 };
             }
@@ -254,6 +260,36 @@ namespace CycloneDX.Utils
                 {
                     result.Metadata.Tools.Tools.AddRange(bom.Metadata.Tools.Tools);
                 }
+                if (bom.Metadata?.Tools?.Components?.Count > 0)
+                {
+                    if (result.Metadata.Tools.Components == null)
+                    {
+                        result.Metadata.Tools.Components = new List<Component>();
+                    }
+                    foreach (var component in bom.Metadata.Tools.Components)
+                    {
+                        NamespaceComponentBomRefs(ComponentBomRefNamespace(bom.Metadata.Component), component);
+                        if (!result.Metadata.Tools.Components.Contains(component))
+                        {
+                            result.Metadata.Tools.Components.Add(component);
+                        }
+                    }
+                }
+                if (bom.Metadata?.Tools?.Services?.Count > 0)
+                {
+                    if (result.Metadata.Tools.Services == null)
+                    {
+                        result.Metadata.Tools.Services = new List<Service>();
+                    }
+                    foreach (var service in bom.Metadata.Tools.Services)
+                    {
+                        service.BomRef = NamespacedBomRef(bom.Metadata.Component, service.BomRef);
+                        if (!result.Metadata.Tools.Services.Contains(service))
+                        {
+                            result.Metadata.Tools.Services.Add(service);
+                        }
+                    }
+                }
 
                 var thisComponent = bom.Metadata.Component;
                 if (thisComponent.Components is null) bom.Metadata.Component.Components = new List<Component>();
@@ -345,6 +381,11 @@ namespace CycloneDX.Utils
 
         private static void NamespaceComponentBomRefs(Component topComponent)
         {
+            NamespaceComponentBomRefs(ComponentBomRefNamespace(topComponent), topComponent);
+        }
+
+        private static void NamespaceComponentBomRefs(string bomRefNamespace, Component topComponent)
+        {
             var components = new Stack<Component>();
             components.Push(topComponent);
 
@@ -353,12 +394,14 @@ namespace CycloneDX.Utils
                 var currentComponent = components.Pop();
 
                 if (currentComponent.Components != null)
-                foreach (var subComponent in currentComponent.Components)
                 {
-                    components.Push(subComponent);
+                    foreach (var subComponent in currentComponent.Components)
+                    {
+                        components.Push(subComponent);
+                    }
                 }
 
-                currentComponent.BomRef = NamespacedBomRef(topComponent, currentComponent.BomRef);
+                currentComponent.BomRef = NamespacedBomRef(bomRefNamespace, currentComponent.BomRef);
             }
         }
 
