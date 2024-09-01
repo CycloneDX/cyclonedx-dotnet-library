f6ea47bdaa69eaebfbc995fc8ecd6f6b3e4ae5a6
MTsfoni <mibau89@gmail.com>
2024-09-01 20:33:30 +0200

    missing added file
    Signed-off-by: MTsfoni <mibau89@gmail.com>

---
+++ src/CycloneDX.Core/Models/Interfaces/IHasBomRef.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CycloneDX.Models
+{
+    public interface IHasBomRef
+    {
+        string BomRef { get; set; }
+    }
+}
